45d66a621dd2b195c014d873dbc6d92c00e28fd8
Nicolai Berk <nicolai.berk@gmail.com>
2022-11-29 16:12:43 +0100

    Get party manifestos

---
+++ code/preprocessing/get_manifestos.R
@@ -0,0 +1,51 @@
+# ______________________________________________
+# FrameCompetition
+# Goal: Create set of manifestos
+# Procedure: Get data, generatae dataset, save
+# ______________________________________________
+# Date:  Tue Nov 29 15:34:54 2022
+# Author: Nicolai Berk
+#  R version 4.1.1 (2021-08-10)
+# ______________________________________________
+
+# parameters ####
+clean_list <- c()
+
+
+# Get data ####
+mp_setapikey("data/raw/manifestos/manifesto_apikey.txt")
+
+my_corpus <- mp_corpus(countryname == "Germany" & edate > as.Date("2020-01-01"))
+
+#
+mpds <- mp_maindataset()
+
+# generatae dataset ####
+corpus <- data.frame(doc_id = NULL, text = NULL)
+
+for (doc in names(my_corpus)){
+  
+  text <- content(my_corpus[[doc]])
+  partycode <- meta(my_corpus[[doc]])$party
+  partyname <- mpds[mpds$party == partycode, "partyabbrev"] %>% unique() %>% .[[1]]
+  
+  corpus <- 
+    
+    rbind(
+      corpus,
+      data.frame(
+        doc_id = partyname,
+        text = text
+      )
+    )
+}
+
+# save ####
+fwrite(corpus, "data/raw/manifestos/manifestos.csv")
+
+# clean up ####
+rm(list = clean_list)
+
+
+
+
