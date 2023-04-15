5c2f879fb1f8050f2af404e4f178423657ddc75f
Nicolai Berk <nicolai.berk@gmail.com>
2023-04-15 14:58:28 +0200

    Collect Migration-Related Manifesto Content

--- code/collection/get_manifestos.R
+++ code/collection/get_manifestos.R
@@ -8,43 +8,58 @@
 #  R version 4.1.1 (2021-08-10)
 # ______________________________________________
 
-# parameters ####
-whitelist <- ls()
+# setup
+library(tidyverse)
+library(dplyr)
+library(manifestoR)
+library(data.table)
 
-
-# Get data ####
+# Get data 13 & 17 elections ####
 mp_setapikey("data/raw/manifestos/manifesto_apikey.txt")
 
-my_corpus <- mp_corpus(countryname == "Germany" & 
-                         edate < as.Date("2014-01-01") & 
+my_corpus <- mp_corpus(countryname == "Germany" &
+                         edate < as.Date("2018-01-01") &
                          edate > as.Date("2013-01-01"))
 
-#
-mpds <- mp_maindataset(); clean_list <- c(clean_list, "mpds")
+mpds <- mp_maindataset()
 
-# generatae dataset ####
-corpus <- data.frame(doc_id = NULL, text = NULL)
+# generatae migration corpus ####
+corpus <- 
+  data.frame(
+    party = NULL,
+    date = NULL,
+    text = NULL, 
+    code = NULL)
 
 for (doc in names(my_corpus)){
-  
+
   text <- content(my_corpus[[doc]])
+  yr <- meta(my_corpus[[doc]])$date %>% 
+        as.character() %>% 
+        str_sub(1,4)
+  mp_codes <- codes(my_corpus[[doc]])
   partycode <- meta(my_corpus[[doc]])$party
   partyname <- mpds[mpds$party == partycode, "partyabbrev"] %>% unique() %>% .[[1]]
   
   corpus <- 
-    
     rbind(
       corpus,
       data.frame(
-        doc_id = partyname,
+        party = partyname,
+        date = yr,
+        code = mp_codes,
         text = text
       )
     )
 }
 
 # save ####
-fwrite(corpus, "data/raw/manifestos/manifestos.csv")
+fwrite(corpus, "data/raw/manifestos/manifestos1317.csv")
 
-# clean up ####
-rm(ls()[!ls() %in% whitelist])
+# mig texts only
+mig_corpus <- corpus %>%
+  filter(code %in% c("601.2", "602.2", "607.2", "608.2")) # immigration-related codes acc to CMP v5 only available for 2017
 
+table(mig_corpus$party, mig_corpus$code, mig_corpus$date) # clearly separating positions
+
+fwrite(mig_corpus, "data/raw/manifestos/manifestos1317_mig.csv")
